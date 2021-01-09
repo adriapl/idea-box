745ddd72b226404518f1f08b437dd2c2664faf32
Adrián Pérez López <a.przlopez@gmail.com>
2021-01-09 12:15:01 +0100

    feat: create auth with firebase

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,20 @@
             placeholder="Add your idea"
           />
           <input
+            v-if="user"
             class="w-full p-2 bg-gray-600 text-white sm:flex-1"
             type="submit"
             value="Add idea"
           />
         </form>
+        <p class="user-actions" v-if="!user">
+          Please
+          <a @click="doLogin" href="#">login</a>
+          first
+        </p>
+        <p class="user-actions" v-else>
+          Hi {{ user.displayName }}.<a @click="doLogout" href="#"> Logout</a>.
+        </p>
       </section>
       <AppIdea v-for="(idea, $index) in ideas" :key="$index" :idea="idea" />
     </div>
@@ -26,14 +35,49 @@
 import AppIdea from "./components/AppIdea";
 import mock from "@/mock.json";
 import { ref } from "vue";
+import { auth, firebase } from "@/firebase.js";
 export default {
   name: "App",
   setup() {
     const ideas = ref(mock.ideas);
-    return { ideas };
+
+    let user = ref(null);
+    auth.onAuthStateChanged(async (auth) => (user.value = auth ? auth : null));
+
+    const doLogin = async () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+
+      try {
+        await auth.signInWithPopup(provider);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const doLogout = async () => {
+      try {
+        await auth.signOut();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return { ideas, user, doLogin, doLogout };
   },
   components: {
     AppIdea
   }
 };
 </script>
+
+<!-- If I remove the lang="postcss" an error appears "semi-colon expectedcss(css-semicolonexpected)" -->
+<style lang="postcss">
+.user-actions {
+  @apply mt-2 text-center;
+}
+
+.user-actions a {
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
